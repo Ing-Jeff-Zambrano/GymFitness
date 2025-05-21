2beb24b1a68709441a8cdee71d0722ca4aeb578c
Ing-Jeff-Zambrano <jzambranoj8@unemi.edu.ec>
2025-05-21 17:03:36 -0500

    Realice el template register y el modelo de usuario

--- Perfil/models.py
+++ Perfil/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    fecha_nacimiento = models.DateField()
+    sexo = models.CharField(
+        max_length=1,
+        choices=(
+            ('M', 'Masculino'),
+            ('F', 'Femenino'),
+            ('O', 'Otro'),
+        )
+    )
+    direccion = models.CharField(max_length=255, blank=True, null=True)
+    telefono = models.CharField(max_length=20, blank=True, null=True)
+    github_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    google_id = models.CharField(max_length=255, unique=True, blank=True, null=True)
+    tipo_cuerpo = models.CharField(max_length=50, blank=True, null=True)
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name=('groups'),
+        blank=True,
+        help_text=(
+            'The groups this user belongs to. A user will get all permissions '
+            'granted to each of their groups.'
+        ),
+        related_name="gym_fitness_user_set",  # Añade un related_name único
+        related_query_name="user",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name=('user permissions'),
+        blank=True,
+        help_text=('Specific permissions for this user.'),
+        related_name="gym_fitness_user_perm_set",  # Añade un related_name único
+        related_query_name="user",
+    )
+
+    def __str__(self):
+        return self.username
--- Perfil/views.py
+++ Perfil/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 def login_view(request):
     form = AuthenticationForm()
     return render(request, 'perfil/login.html', {'form': form})
 
-
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in the user after successful registration
+            messages.success(request, 'Account created successfully!')
+            return redirect('some_dashboard_url') # Redirect to a dashboard or home page
+        else:
+            for error in form.errors.values():
+                messages.error(request, error)
+    else:
+        form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'perfil/register.html', context)
