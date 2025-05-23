006c6417925dbbcd2111556baa2341014f979c88
Ing-Jeff-Zambrano <jzambranoj8@unemi.edu.ec>
2025-05-23 16:25:29 -0500

    Realice el dashboard y cree la vista logout

--- Perfil/urls.py
+++ Perfil/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('logout/', views.logout, name='logout'),
 ]
