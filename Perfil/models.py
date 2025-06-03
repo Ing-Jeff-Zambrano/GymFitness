374f821bd7b7fd60db916fddffe630a2a755e67d
Ing-Jeff-Zambrano <jzambranoj8@unemi.edu.ec>
2025-06-03 11:14:22 -0500

    Se corrigió el campo fecha_nacimiento en el modelo Usuario para permitir nulos

--- Perfil/models.py
+++ Perfil/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
-    fecha_nacimiento = models.DateField()
+    fecha_nacimiento = models.DateField(null=True, blank=True)
     sexo = models.CharField(
         max_length=1,
         choices=(
